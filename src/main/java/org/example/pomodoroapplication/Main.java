966086651a10a6699128b47d41ceb248655c8996
Nino Anthony <talisikninoanthony@gmail.com>
2025-03-16 13:38:30 +0800

    added css style

--- src/main/java/org/example/pomodoroapplication/Main.java
+++ src/main/java/org/example/pomodoroapplication/Main.java
@@ -3,16 +3,20 @@ package org.example.pomodoroapplication;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+        String css = Objects.requireNonNull(getClass().getResource("/css/styles.css")).toExternalForm();
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/PomoductiveLogo.png"))));
+        stage.setTitle("Pomoductive");
         stage.setScene(scene);
         stage.show();
     }
